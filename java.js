357c069e292b04efa270601891095847a0a7e229
roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-08-06 00:44:45 +0900

    Add files via upload

--- java.js
+++ java.js
@@ -1,22 +1,13 @@
-//const loginForm = document.getElementById("login-form");
-//const ioginInput = loginForm.querySelector("input");
-//const ioginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const logininput = document.querySelector("#login-form input");
 
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+function onloginsubmit(evento){
+evento.preventDefault();
+   console.log(logininput.value);
 
-function loginclick(){
-   const username = loginInput.value;
-   if (username === ""){
-    alert("plz write your right name");
-   } else if (username.length > 15){
-    alert("your name is too long");
-   } else {
-    alert("hi username");
-   }
-   
 }
+
 //이렇게 하는것도 좋지만 우리는 browser에 있는 기능을 사용하길 선호한다.//
 
 
-loginButton.addEventListener("click",loginclick);
+loginForm.addEventListener("submit", onloginsubmit);
