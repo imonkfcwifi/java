abeb99eba9208ead86443490855890d2283ba470
Roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-08-07 22:04:48 +0900

    Add files via upload

--- java.js
+++ java.js
@@ -1,13 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const logininput = document.querySelector("#login-form input");
 
-function onloginsubmit(evento){
-evento.preventDefault();
-   console.log(logininput.value);
+const link = document.querySelector("a");
+function onloginsubmit(event){
+event.preventDefault();
+   console.log(event);
 
 }
 
 //이렇게 하는것도 좋지만 우리는 browser에 있는 기능을 사용하길 선호한다.//
 
-
+function handlelinkclicker(event){
+   event.preventDefault();
+console.dir(event);
+ alert("clicked");
+}
 loginForm.addEventListener("submit", onloginsubmit);
+link.addEventListener("click", handlelinkclicker);
+//addEventListener의 함수는 직접 실행이 아닌 브라우저에 내장되어있는 것을 실행 시킨 것 이다.
