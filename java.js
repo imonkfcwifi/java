cb856a215073512ed9aacb14ce0d46f81620a766
Roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-08-08 10:18:35 +0900

    Add files via upload

--- java.js
+++ java.js
@@ -1,20 +1,23 @@
 const loginForm = document.querySelector("#login-form");
-const logininput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
-function onloginsubmit(event){
-event.preventDefault();
-   console.log(event);
+function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+//일반적으로 string만 포함된 변수는 대문자로 표기하고
+//string을 저장하고 싶을때 사용함.
+//그리고 위 HIDDEN CLASSNAME은 loginform이나 logininput처럼 중요한 정보를 담은것이 아니라 대문자로 작성함.
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  console.log(username);
+  //greeting.innerText = "Hello" + username;
+  greeting.innerText = `Hello ${username}`
 
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-//이렇게 하는것도 좋지만 우리는 browser에 있는 기능을 사용하길 선호한다.//
-
-function handlelinkclicker(event){
-   event.preventDefault();
-console.dir(event);
- alert("clicked");
-}
-loginForm.addEventListener("submit", onloginsubmit);
-link.addEventListener("click", handlelinkclicker);
-//addEventListener의 함수는 직접 실행이 아닌 브라우저에 내장되어있는 것을 실행 시킨 것 이다.
+loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
