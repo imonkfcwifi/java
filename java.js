7bbd91cc8cdf4a0a80b1c956b4cf4497dd7b2825
Roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-08-08 10:30:15 +0900

    Add files via upload

--- java.js
+++ java.js
@@ -1,9 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
 const HIDDEN_CLASSNAME = "hidden";
 //일반적으로 string만 포함된 변수는 대문자로 표기하고
 //string을 저장하고 싶을때 사용함.
@@ -12,12 +10,10 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
   //greeting.innerText = "Hello" + username;
   greeting.innerText = `Hello ${username}`
 
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
 loginForm.addEventListener("submit", onLoginSubmit);
