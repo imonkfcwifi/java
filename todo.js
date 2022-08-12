c0f08ac79d3ecbe836ceae843121a84db6ba5d46
Roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-08-12 15:19:09 +0900

    Add files via upload
    array 초기화 억제

--- todo.js
+++ todo.js
@@ -2,7 +2,10 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 // = todoForm.querySelector("input");
-const toDos = [];
+let toDos = [];
+//const toDos = [];의 값이 항상 비어있기 때문에 초기화 현상이 발생했다.
+    //newTodo를 빈 array였던 toDos array에 push 했기에 계속 추가할때마다 초기화됐던것
+    //그래서 let으로 가변수로 바꾼다, 그럼 모든게 다 해결
 const TODOS_KEY = "todos";
 //local storage는 text만 저장가능
 
@@ -22,8 +25,26 @@ function handleToDoSubmit(event){
     //input의 value를 새로운 변수에 복사하는 것이기 때문에 value의 값이 empty가 되더라도 값은 newTodo 저장되어 남아있다.
 }
 
-toDoForm.addEventListener("submit",handleToDoSubmit);
 
+function sayhello(item){
+    console.log("this is the turn of",item);
+}
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+console.log(savedTodos);
+if (savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    toDos = parsedTodos;
+    //toDos에 parsedToDos를 넣어서 전에 있던 toDo를 복원
+    parsedTodos.forEach(paintTodo);
+    //sayhello("a"),sayhello("b") . . .를 하나씩 실행한다
+    //array 이기에 가능한 일이다.
+    //storage에 있는 item들을 paintTodo에 넣어서 실행시키면 저장된 값이 나옴
+
+}
+//array는 가장 중요한 data type, 각각의 item에 대해 function 가능
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
 
 function paintTodo(newTodo){
 
@@ -45,22 +66,8 @@ function paintTodo(newTodo){
 
 function deleteToDo(event){
     const removeParent = event.target.parentElement;
+
     removeParent.remove();
     //어떤 버튼을 지울건지 정해줘야함. 그냥 버튼으로 해놓으면 하나만 눌러도 모든 button을 인식해서 function을 실행시켜버림
 }
 
-function sayhello(item){
-    console.log("this is the turn of",item);
-}
-
-const savedTodos = localStorage.getItem(TODOS_KEY);
-console.log(savedTodos);
-if (savedTodos !== null){
-    const parsedTodos = JSON.parse(savedTodos);
-    parsedTodos.forEach(sayhello);
-    //sayhello("a"),sayhello("b") . . .
-    //array 이기에 가능한 일이다.
-
-}
-
-//array는 가장 중요한 data type, 각각의 item에 대해 function 가능
